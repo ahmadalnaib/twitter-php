418022435d41da7eb31fe6caf426a8197b135e07
ahmedhabeeb73 <alnaib888@gmail.com>
2021-09-01 15:46:21 +0200

    add js

--- sign.php
+++ sign.php
@@ -52,5 +52,42 @@
   </form>
   </div>
   </div>
+
+
+  <script
+			  src="https://code.jquery.com/jquery-3.6.0.min.js"
+			  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+			  crossorigin="anonymous"></script>
+        <script>
+          for(i = new Date().getFullYear(); i > 1900; i--){
+            $("#years").append($('<option/>').val(i).html(i));
+          }
+
+          for(i = 1; i<13; i++){
+           $('#months').append($('<option/>').val(i).html(i));
+          }
+
+        
+
+         updateNumberOfDays();
+         function updateNumberOfDays()
+         {
+           $('#days').html('');
+            month=$('#months').val();
+           year= $('#years').val();
+            days =daysInMonth(month,year);
+
+            for(i=1; i <days+1; i++){
+              $('#days').append($('<option/>').val(i).html(i));
+            }
+         }
+
+           $('#years,#months').on('change',function(){
+             updateNumberOfDays();
+           })
+         function daysInMonth(month,year){
+           return new Date(year,month,0).getDate()
+         }
+        </script>
 </body>
 </html>
